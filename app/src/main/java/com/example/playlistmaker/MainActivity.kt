ec5a991d8ea0bb0e9de298551351ce76e557488a
Capcan <lawyer@shultzlaw.ru>
2024-06-03 21:05:11 +0300

    Создал главный экран и экран настроек. Реализовал переход по клику на кнопку настройки. Реализовал переход назад из экрана настроек.

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,20 +1,46 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+//        val search = findViewById<Button>(R.id.search)
+//        val mediatek = findViewById<Button>(R.id.mediatek)
+        val settings = findViewById<Button>(R.id.settings)
+
+        settings.setOnClickListener {
+            val displayIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(displayIntent)
+//
+//            search.setOnClickListener(this@MainActivity)
+//            mediatek.setOnClickListener(this@MainActivity)
+
+//        val imageClickListener: View.OnClickListener = View.OnClickListener { Toast.makeText(this@MainActivity, "Нажали на картинку!", Toast.LENGTH_SHORT).show() }
+//
+//        image.setOnClickListener(imageClickListener)
         }
+
+//        override fun onClick(p0: View?) {
+//            when (p0?.id) {
+//                R.id.search -> {
+//                    Toast.makeText(this, "Сработала кнопка поиска", Toast.LENGTH_SHORT).show()
+//                }
+//
+//                R.id.mediatek -> {
+//                    Toast.makeText(this, "Сработала кнопка медиатеки", Toast.LENGTH_SHORT).show()
+//                }
+//
+//            }
+//
+//
+//        }
     }
 }
---
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -0,0 +1,29 @@
+package com.example.playlistmaker
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Toolbar
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.playlistmaker.ui.theme.PlaylistMakerTheme
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+    }
+    fun onClickClose(view: View) {
+        finish()
+    }
+}
