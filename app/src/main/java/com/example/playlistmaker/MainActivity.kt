0166792347ec7c3f561b0e5ee1eaceee9150c9cf
Capcan <lawyer@shultzlaw.ru>
2024-06-04 18:54:04 +0300

    1. Создал класс SettingsActivity. Объявил его в AndroidManifest.xml. 2. Установил для SettingsActivity разметку. 3. Создал две пустых Activity для медиатеки и для поиска. Объявил их в AndroidManifest.xml. 4. Убрал код Toast из обработчиков нажатия на кнопки. 5. Добавил код переходов на экраны в соответствующие обработчики. 6. Для кнопки «Назад» экрана «Настройки» добавил обработчик нажатий. Добавил код перехода на главный экран в обработчик.

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -12,35 +12,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        val search = findViewById<Button>(R.id.search)
-//        val mediatek = findViewById<Button>(R.id.mediatek)
+        val search = findViewById<Button>(R.id.search)
+        val mediateca = findViewById<Button>(R.id.mediateca)
         val settings = findViewById<Button>(R.id.settings)
 
         settings.setOnClickListener {
-            val displayIntent = Intent(this, SettingsActivity::class.java)
-            startActivity(displayIntent)
-//
-//            search.setOnClickListener(this@MainActivity)
-//            mediatek.setOnClickListener(this@MainActivity)
+            val settingsIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(settingsIntent)
 
-//        val imageClickListener: View.OnClickListener = View.OnClickListener { Toast.makeText(this@MainActivity, "Нажали на картинку!", Toast.LENGTH_SHORT).show() }
-//
-//        image.setOnClickListener(imageClickListener)
         }
-
-//        override fun onClick(p0: View?) {
-//            when (p0?.id) {
-//                R.id.search -> {
-//                    Toast.makeText(this, "Сработала кнопка поиска", Toast.LENGTH_SHORT).show()
-//                }
-//
-//                R.id.mediatek -> {
-//                    Toast.makeText(this, "Сработала кнопка медиатеки", Toast.LENGTH_SHORT).show()
-//                }
-//
-//            }
-//
-//
-//        }
     }
 }
